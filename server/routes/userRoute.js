3db5922b5c096464b3b8e132605263e0d6a3eadb
Soundararajan-J <soundararajanj@resileo-labs.com>
2024-11-18 01:15:37 +0530

    get to post

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const { authenticate } = require("../middleware/authMiddleware");
 
-router.get("/getUserDetails", authenticate, userController.getUserDetails);
+router.post("/getUserDetails", authenticate, userController.getUserDetails);
 router.post("/uploadAvatar", authenticate, userController.uploadAvatar);
 module.exports = router;
